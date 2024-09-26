00c19e6a5d678075e58e484fd46317303164a246
Josue Perez <josuepe03@hotmail.com>
2024-09-26 17:55:40 -0600

    UPDATE HOME

--- src/public/pages/PublicPage.jsx
+++ src/public/pages/PublicPage.jsx
@@ -4,11 +4,11 @@ import Navbar from "../components/Navbar";
 export default function PublicPage() {
   return (
     <>
-      <article className="h-full sm:h-screen  public_gradient ">
-        <Navbar />
+      <main className="h-full sm:h-screen   ">
 
-        <section className="grid lg:grid-cols-2  lg:h-full  overflow-x-hidden">
-          <div className=" px-6 flex flex-col lg:justify-center pt-10 lg:-mt-20  ">
+        <section className="grid lg:grid-cols-2  lg:h-full  overflow-x-hidden public_gradient">
+        <Navbar />
+          <article className=" px-6 flex flex-col lg:justify-center pt-10 lg:-mt-20  ">
             <span className="text-gray-400 mb-3">Sistema v1.0 {">"}</span>
 
             <h1 className="mb-4 text-3xl font-extrabold text-gray-100  md:text-5xl xl:text-6xl">
@@ -48,7 +48,7 @@ export default function PublicPage() {
                 </svg>
               </Link>
             </div>
-          </div>
+          </article>
           <div className=" flex justify-end  items-center ">
             <img
               className="h-[85%] lg:h-[50%] xl:h-[60%] rounded-s-lg -mr-20 sm:m-0 "
@@ -57,7 +57,136 @@ export default function PublicPage() {
             />
           </div>
         </section>
-      </article>
+
+        <section className=" py-20">
+          <header className="w-[70%] mx-auto text-center">
+            <h2 className="text-4xl font-extrabold text-gray-900 ">
+              Servicios
+            </h2>
+            <p className="text-gray-600 font-medium mt-6">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Perferendis molestiae facilis eum asperiores amet libero ipsam ea
+              atque laboriosam cupiditate porro sunt excepturi, voluptate odit
+              iste itaque animi doloremque molestias.
+            </p>
+          </header>
+          <article className="headline grid md:grid-cols-2 lg:grid-cols-3 w-[70%] md:w-[90%] xl:w-[70%]  mx-auto gap-10 mt-20">
+            {/* <!-- CARD1 --> */}
+            <div className=" flex flex-col  ">
+              <div className="bg-cyan-600 w-fit p-1 rounded-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-user text-gray-100"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg>
+              </div>
+              <a className="text-xl font-bold my-2">
+                <u> Gestion Clientes </u>
+              </a>
+              <p className="text-gray-900  text-sm">
+                Un diseño impactante puede ayudarle a hacer crecer su empresa y
+                a vender productos de forma más eficaz.
+              </p>
+
+              <a href="" className="text-sm text-blue-800 font-bold mt-5">
+                Ver mas...
+              </a>
+            </div>
+            {/* <!-- CARD 2 --> */}
+            <div className=" flex flex-col  ">
+              <div className="bg-cyan-600 w-fit p-1 rounded-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-user text-gray-100"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg>
+              </div>
+              <a className="text-xl font-bold my-2">
+                <u> Gestion Equipos </u>
+              </a>
+              <p className="text-gray-900  text-sm">
+                Un diseño impactante puede ayudarle a hacer crecer su empresa y
+                a vender productos de forma más eficaz.
+              </p>
+
+              <a href="" className="text-sm text-blue-800 font-bold mt-5">
+                Ver mas...
+              </a>
+            </div>
+            {/* <!-- CARD 3 --> */}
+            <div className=" flex flex-col  ">
+              <div className="bg-cyan-600 w-fit p-1 rounded-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-user text-gray-100"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg>
+              </div>
+              <a className="text-xl font-bold my-2">
+                <u> Gestion Reparaciones </u>
+              </a>
+              <p className="text-gray-900  text-sm">
+                Un diseño impactante puede ayudarle a hacer crecer su empresa y
+                a vender productos de forma más eficaz.
+              </p>
+
+              <a href="" className="text-sm text-blue-800 font-bold mt-5">
+                Ver mas...
+              </a>
+            </div>
+          </article>
+        </section>
+
+        <section className="py-20">
+          <header className="w-[70%] mx-auto text-center">
+            <h2 className="text-4xl font-extrabold text-gray-900 ">
+              Dashboard
+            </h2>
+            <p className="text-gray-600 font-medium mt-6">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Perferendis molestiae facilis eum asperiores amet libero ipsam ea
+              atque laboriosam cupiditate porro sunt excepturi, voluptate odit
+              iste itaque animi doloremque molestias.
+            </p>
+          </header>
+          <article>
+
+          </article>
+        </section>
+      </main>
     </>
   );
 }
